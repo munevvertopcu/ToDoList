fe1e241699039c938044779319f8d85e46d1da0f
munevver <munevvertopcu98@gmail.com>
2022-05-08 22:42:49 +0300

    Input box style changed

--- src/components/InputBox/InputBox.style.js
+++ src/components/InputBox/InputBox.style.js
@@ -2,19 +2,12 @@ import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
    container:{
-    //    backgroundColor: '#37474f',
-    //    marginHorizontal: 10,
-    //    marginBottom:10,
-    //    borderRadius: 10,
-    //    padding: 5,
     backgroundColor: "#37474f",
     padding: 15,
     margin: 10,
     borderRadius: 10,
    },
    input:{
-    //    backgroundColor: '#00ffff',
-    //    height: 40
     borderBottomWidth: 1,
     borderBottomColor: "#fff",
     color: "#fff",
@@ -22,11 +15,6 @@ export default StyleSheet.create({
     marginBottom: 10,
    },
    button:{
-    //    backgroundColor: '#dcdcdc',
-    //    marginTop: 5,
-    //    alignItems: 'center',
-    //    padding: 5,
-    //    borderRadius: 10
     backgroundColor: "#ffa500",
     padding: 10,
     marginRight: 13,
